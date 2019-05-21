74fec543ef828d9eba84e462fb2b5cb501468ba7
Xavier Michelon <xmichelo@users.noreply.github.com>
2019-05-21 07:25:09 +0200

    Updated version number to 2.1.

--- Beeftext/BeeftextConstants.cpp
+++ Beeftext/BeeftextConstants.cpp
@@ -17,7 +17,7 @@ namespace constants {
 QString const kApplicationName = "Beeftext";
 QString const kOrganizationName = "beeftext.org";
 qint32 const kVersionMajor = 2;
-qint32 const kVersionMinor = 0;
+qint32 const kVersionMinor = 1;
 QString const kBeeftextWikiHomeUrl = "https://github.com/xmichelo/Beeftext/wiki";
 QString const kGettingStartedUrl = "https://github.com/xmichelo/Beeftext/wiki/Getting-started";
 QString const kBeeftextWikiVariablesUrl = "https://github.com/xmichelo/Beeftext/wiki/Variables";
