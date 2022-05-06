87c4573a8c64bc5f1cb17d8da29738897a8f2b0f
Xavier Michelon <xavier@michelon.ch>
2022-05-06 07:25:35 +0200

    Fixed picker windows background with not custom theme.
    Fixes #491.

--- Beeftext/Theme.cpp
+++ Beeftext/Theme.cpp
@@ -12,6 +12,8 @@
 #include "BeeftextGlobals.h"
 #include <XMiLib/Exception.h>
 
+QString loadStylesheetFile(QString const& path); ///< Load a stylesheet from file. The function throws an exception if an error occur.
+
 
 //**********************************************************************************************************************
 /// \param[in] theme The theme.
@@ -86,28 +88,13 @@ void applyThemePreferences(bool useCustomTheme, ETheme theme)
    {
       if (!useCustomTheme)
       {
-         qApp->setStyleSheet(QString());
+         qApp->setStyleSheet(loadStylesheetFile(":/MainWindow/Resources/StyleNoCustom.qss"));
          return;
       }
 
-      // Load the common part of the stylesheet.
-      QString const resourcePath;
-      QFile commonFile(":/MainWindow/Resources/StyleCommon.qss");
-      if (!commonFile.open(QIODevice::ReadOnly | QIODevice::Text))
-         throw xmilib::Exception(QString("Could not load stylesheet from resource '%1'").arg(resourcePath));
-      QString stylesheet = QString::fromUtf8(commonFile.readAll()) + "\n";
-      commonFile.close();
-
-      // Add the theme-dependent part of the style sheet.
-      QString const themeResourcePath = QString(":/MainWindow/Resources/Style%1.qss").arg((ETheme::Dark == theme)
-         ? "Dark" : "Light");
-      QFile themeFile(themeResourcePath);
-      if (!themeFile.open(QIODevice::ReadOnly | QIODevice::Text))
-         throw xmilib::Exception(QString("Could not load stylesheet from resource '%1'").arg(resourcePath));
-      stylesheet += QString::fromUtf8(themeFile.readAll());
-
-      qApp->setStyleSheet(stylesheet);
-
+      qApp->setStyleSheet(loadStylesheetFile(":/MainWindow/Resources/StyleCommon.qss") + "\n" + 
+         loadStylesheetFile( QString(":/MainWindow/Resources/Style%1.qss").arg((ETheme::Dark == theme) 
+            ? "Dark" : "Light")));
    }
    catch (xmilib::Exception const& e)
    {
@@ -119,3 +106,15 @@ void applyThemePreferences(bool useCustomTheme, ETheme theme)
 
 }
 
+
+//**********************************************************************************************************************
+/// \param[in] path The path of the style sheet to load.
+/// \return The loaded style sheet.
+//**********************************************************************************************************************
+QString loadStylesheetFile(QString const& path)
+{
+   QFile file(path);
+   if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+      throw xmilib::Exception(QString("Could not load stylesheet %1").arg(path));
+   return QString::fromUtf8(file.readAll());
+}
